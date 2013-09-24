ee887407665905a450ff730b516eb1a4defc5542
Amy Ton <amyhton@gmail.com>
2013-09-24 10:25:16 -0500

    Add new URL, route, method to controller, and view page for Air Force One.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,7 @@
 class VideosController < ApplicationController
   def show_gladiator
   end
+
+  def show_air_force_one
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Metube::Application.routes.draw do
   get "/videos/gladiator" => "videos#show_gladiator"
+  get "/videos/air_force_one" => "videos#show_air_force_one"
 end
